9051a928d621c4557501be78ef332463d8f20e97
Leigh Akin <leigh.akin@ttagit.com>
2016-02-08 17:23:13 -0800

    map marker

--- mapcode.js
+++ mapcode.js
@@ -108,11 +108,6 @@
     }
 ]
 
-                     [{stylers:[{hue:'#2c3e50'},{saturation:250}]},{featureType:'road',elementType:'geometry',stylers:[{lightness:50},{visibility:'simplified'}]},{featureType:'road',elementType:'labels',stylers:[{visibility:'off'}]}]
-                };
-
-
-
 		var mapElement = document.getElementById('map');
 
                 // Create the Google Map using out element and options defined above
